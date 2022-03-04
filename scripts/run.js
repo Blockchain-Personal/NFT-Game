17055501e3b116e913a0c51e2f078ff4ae80de84
rya-sge <ryan.sauge@heig-vd.ch>
2022-03-04 01:07:53 +0100

    Create characters

--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,13 @@
 const main = async () => {
     const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
-    const gameContract = await gameContractFactory.deploy();
+    const gameContract = await gameContractFactory.deploy(
+        ["KernelPanic", "InfiniteLoop", "SegmentationFault"],       // Names
+        ["https://github.com/rya-sge/myNFTCollection/blob/main/coll2/InfinieLoop.png", // Images
+            "https://github.com/rya-sge/myNFTCollection/blob/main/coll2/KernelPanic.png",
+            "https://github.com/rya-sge/myNFTCollection/blob/main/coll2/SegmentationFault.png"],
+        [100, 200, 300],                    // HP values
+        [100, 60, 35]                       // Attack damage values
+    );
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
 };
